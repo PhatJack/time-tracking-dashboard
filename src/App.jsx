5ae09d04a8078dfcfbbeb8d8dc6a7f066d0a9c6f
PhatJack <tienphatng.693@gmail.com>
2024-01-18 21:57:52 +0700

    update solution without responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React, { useEffect, useState } from 'react'
+import Avatar from '../images/image-jeremy.png'
+import Ellipsis from '../images/icon-ellipsis.svg'
+import Exercise from '../images/icon-exercise.svg'
+import Play from '../images/icon-play.svg'
+import Study from '../images/icon-study.svg'
+import Social from '../images/icon-social.svg'
+import Work from '../images/icon-work.svg'
+import SelfCare from '../images/icon-self-care.svg'
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const [active, setActive] = useState('weekly')
+	const svgImg = [Work, Play, Study, Exercise, Social, SelfCare]
+	const [data, setData] = useState([])
+
+	const getData = async () => {
+		await fetch('../data.json')
+			.then((res) => res.json())
+			.then(data => {
+				setData(data)
+			})
+	}
+	useEffect(() => {
+		getData()
+	}, [])
+	return (
+		<>
+			<div className="w-full h-screen flex justify-center items-center bg-very-dark-blue">
+				<div className="max-w-[1143px] w-full min-h-[520px] md:h-fit h-dvh grid justify-center grid-cols-custom-desktop grid-rows-custom-desktop gap-[1.875rem]">
+					<div className="bg-dark-blue cols-[1_/_2] row-[1_/_-1] rounded-[1.15rem] relative">
+						<div className="bg-blue h-[350px] w-full flex flex-col gap-10 p-8 rounded-[1.15rem]">
+							<img src={Avatar} alt="" className='w-20 h-20 rounded-full border-2 border-white' />
+							<article>
+								<span className='text-pale-blue text-sm'>Report for</span>
+								<h1 className='text-[2.5rem] leading-[3rem] text-white font-light'>Jeremy Robson</h1>
+							</article>
+						</div>
+						<div className="w-full flex flex-col gap-3 p-8">
+							<button type='button' onClick={() => { setActive("daily") }} className={`${active === "daily" ? "text-white" : "text-desatured-blue"} text-lg text-left font-light hover:text-white transition-colors w-fit first-letter:uppercase`}>Daily</button>
+							<button type='button' onClick={() => { setActive("weekly") }} className={`${active === "weekly" ? "text-white" : "text-desatured-blue"} text-lg text-left font-light hover:text-white transition-colors w-fit first-letter:uppercase`}>Weekly</button>
+							<button type='button' onClick={() => { setActive("monthly") }} className={`${active === "monthly" ? "text-white" : "text-desatured-blue"} text-lg text-left font-light hover:text-white transition-colors w-fit first-letter:uppercase`}>Monthly</button>
+						</div>
+					</div>
+					{
+						data.map((item, index) => (
+							<div key={index}
+								className={`${item.title.toLowerCase() === "work" ? "bg-work"
+									: item.title.toLowerCase() === "study" ? "bg-study"
+										: item.title.toLowerCase() === "social" ? "bg-social"
+											: item.title.toLowerCase() === "exercise" ? "bg-exercise"
+												: item.title.toLowerCase() === "self care" ? "bg-self-care" : "bg-play"} rounded-[1rem_1rem_1.25rem_1.25rem] relative w-[255px] h-[244px] flex flex-col justify-end overflow-hidden`}>
+								<img src={svgImg[index]} alt="" className='absolute -top-2 z-[2] right-5' />
+								<section className='w-full h-[200px] bg-dark-blue rounded-2xl	 p-7
+													flex flex-col hover:bg-desatured-blue transition-colors duration-300 z-10'>
+									<div className="w-full flex justify-center items-center">
+										<span className='w-full text-[1.15rem] text-white'>{item.title}</span>
+										<i>
+											<svg width="21" height="5" xmlns="http://www.w3.org/2000/svg">
+												<path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd" />
+											</svg>
+										</i>
+									</div>
+									<article className='flex flex-col gap-2 mt-3 '>
+										<h1 className='text-white font-light text-[3.25rem]'>{item.timeframes[active].current}hrs</h1>
+										<p className='text-pale-blue text-sm'>Last Week - {item.timeframes.weekly.previous}hrs</p>
+									</article>
+								</section>
+							</div>
+						))
+					}
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				"blue": "hsl(246, 80%, 60%)",
+				"work": "hsl(15, 100%, 70%)",
+				"study": "hsl(348, 100%, 68%)",
+				"exercise": "hsl(145, 58%, 55%)",
+				"social": "hsl(264, 64%, 52%)",
+				"play": "hsl(195, 74%, 62%)",
+				"self-care": "hsl(43, 84%, 65%)",
+				"very-dark-blue": "hsl(226, 43%, 10%)",
+				"dark-blue": "hsl(235, 46%, 20%)",
+				"desatured-blue": "hsl(235, 45%, 61%)",
+				"pale-blue": "hsl(236, 100%, 87%)"
+			},
+			gridTemplateColumns: {
+				"custom-desktop": "repeat(auto-fit, minmax(0, 255px))",
+			},
+			gridTemplateRows: {
+				"custom-desktop": "repeat(auto-fit, 244px)"
+			}
+		},
+	},
+	plugins: [],
 }
-
